f238ebd380922b1e830541ae4148d09ac79dc89f
Kautuk Kundan <kautuk@stackrlabs.xyz>
2024-08-22 23:56:05 +0530

    Cleanup unused components and modules (#43)
    * chore: cleanup unused components and modules
    
    * fix: removed deadlink and unused pages

--- docs/layout.tsx
+++ docs/layout.tsx
@@ -1,10 +1,9 @@
 import { ReactNode } from "react";
-import { Web3Provider } from "./components/Web3Provider";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
-  return <Web3Provider>{children}</Web3Provider>;
+  return children;
 }
